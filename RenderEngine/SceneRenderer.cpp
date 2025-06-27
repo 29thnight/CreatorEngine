3ae96d01f43e60f9093615ef1ef504659d0b1483
matstar33 <76931377+matstar33@users.noreply.github.com>
2025-06-27 09:52:57 +0900

    4차머지

--- RenderEngine/SceneRenderer.cpp
+++ RenderEngine/SceneRenderer.cpp
@@ -29,7 +29,6 @@
 #include <regex>
 
 #include "Animator.h"
-
 using namespace lm;
 
 SceneRenderer::SceneRenderer(const std::shared_ptr<DirectX11::DeviceResources>& deviceResources) :
@@ -216,7 +215,7 @@ SceneRenderer::SceneRenderer(const std::shared_ptr<DirectX11::DeviceResources>& 
 
 	m_renderScene->Initialize();
 	m_renderScene->SetBuffers(m_ModelBuffer.Get());
-	//m_pEffectPass = std::make_unique<EffectManager>();
+	m_pEffectPass = std::make_unique<EffectManager>();
 	//m_pEffectPass->MakeEffects(Effect::Sparkle, "asd", float3(0, 0, 0));
     m_newSceneCreatedEventHandle	= newSceneCreatedEvent.AddRaw(this, &SceneRenderer::NewCreateSceneInitialize);
 	m_activeSceneChangedEventHandle = activeSceneChangedEvent.AddLambda([&] 
@@ -379,13 +378,14 @@ void SceneRenderer::OnWillRenderObject(float deltaTime)
 {
 	//
 	//TODO : 이 부분은 PreDepth로 적용해보고 프레임 얼마나 늘어나는지 테스트 필요
-	//m_pEffectPass->Update(deltaTime);
+
 }
 
 void SceneRenderer::EndOfFrame(float deltaTime)
 {
 	//TODO : 시연용 Player주석 코드
 	//player.Update(deltaTime);
+	m_pEffectPass->Update(deltaTime);
 	m_renderScene->EraseRenderPassData();
 	m_renderScene->Update(deltaTime);
 	m_renderScene->OnProxyDistroy();
@@ -625,11 +625,11 @@ void SceneRenderer::SceneRendering()
 		}
 
 		{
-			//DirectX11::BeginEvent(L"EffectPass");
-			//Benchmark banch;
-			//m_pEffectPass->Execute(*m_renderScene, *camera);
-			//RenderStatistics->UpdateRenderState("EffectPass", banch.GetElapsedTime());
-			//DirectX11::EndEvent();
+			DirectX11::BeginEvent(L"EffectPass");
+			Benchmark banch;
+			m_pEffectPass->Execute(*m_renderScene, *camera);
+			RenderStatistics->UpdateRenderState("EffectPass", banch.GetElapsedTime());
+			DirectX11::EndEvent();
 		}
 
 		//[7] SpritePass
