3eabf118b9598345c557ab47f38e80e4294c435f
29thnight <ideneb@naver.com>
2025-09-12 20:58:02 +0900

    feat: add cylindrical billboard support

---
+++ BillboardType.h
@@ -0,0 +1,8 @@
+#pragma once
+
+enum class BillboardType {
+    None = 0,
+    Spherical = 1,
+    Cylindrical = 2
+};
+
--- ScriptBinder/SpriteRenderer.cpp
+++ ScriptBinder/SpriteRenderer.cpp
@@ -2,6 +2,7 @@
 #include "Scene.h"
 #include "RenderScene.h"
 #include "SceneManager.h"
+#include "BillboardType.h"
 
 void SpriteRenderer::Awake()
 {
