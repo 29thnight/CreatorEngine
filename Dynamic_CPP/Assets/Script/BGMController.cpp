897f4e74dedc3950dc02d26975fd22fdd8778cdc
SongSeHwan <sshhss9900@gmail.com>
2025-11-06 17:05:14 +0900

    csv 오타수정

--- Dynamic_CPP/Assets/Script/BGMController.cpp
+++ Dynamic_CPP/Assets/Script/BGMController.cpp
@@ -5,6 +5,11 @@
 #include "SoundComponent.h"
 #include "GameManager.h"
 #include "SFXPoolManager.h"
+void BGMController::Awake()
+{
+	auto instance = GameInstance::GetInstance();
+	nextScene = (SceneType)instance->GetAfterLoadSceneIndex();
+}
 void BGMController::Start()
 {
 	auto instance = GameInstance::GetInstance();
@@ -21,7 +26,14 @@ void BGMController::Start()
 	case SceneType::SelectChar:
 		curSoundName = sounds->GetSoudNameRandom("BgmCharacterSelect");
 		break;
-	case SceneType::Loading:
+	case SceneType::Loading: //·Îµù + ÄÆ¾À  // 1 ,  2  ,3 À¸·Î ³ª´²Á®ÀÖÀ½
+		//if(nextScene == SceneType::Stage)
+
+
+
+
+
+
 		curSoundName = sounds->GetSoudNameRandom("BgmLoad");
 		break;
 	case SceneType::Stage:
