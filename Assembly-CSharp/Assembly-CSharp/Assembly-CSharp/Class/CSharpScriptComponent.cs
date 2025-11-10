9984b498f04d2bfcb07c6ae8007a698f0bd7c80d
29thnight <ideneb@naver.com>
2025-11-10 11:34:27 +0900

    Add CSharpScriptComponent bindings to managed assembly

---
+++ Assembly-CSharp/Assembly-CSharp/Assembly-CSharp/Class/CSharpScriptComponent.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Runtime.CompilerServices;
+
+namespace CreatorEngine
+{
+    public sealed class CSharpScriptComponent : Component
+    {
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private static extern string ICall_GetScriptName(IntPtr self);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private static extern string ICall_GetScriptGuid(IntPtr self);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private static extern bool ICall_HasManagedInstance(IntPtr self);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private static extern uint ICall_GetBehaviorHandle(IntPtr self);
+
+        public string ScriptName => ICall_GetScriptName(m_NativePtr);
+
+        public string ScriptGuid => ICall_GetScriptGuid(m_NativePtr);
+
+        public bool HasManagedInstance => ICall_HasManagedInstance(m_NativePtr);
+
+        public uint BehaviorHandle => ICall_GetBehaviorHandle(m_NativePtr);
+    }
+}
